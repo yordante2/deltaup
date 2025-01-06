6aa93ed9bea0eb2c125c40da20c971ef6fc07b84
yordante <80213792+yordante2@users.noreply.github.com>
2025-01-06 17:19:38 -0500

    Update moodleclient.py

--- moodleclient.py
+++ moodleclient.py
@@ -16,7 +16,7 @@ def upload_token(filename):
   files = {
     "file": (filename, open(filename, "rb"), "application/x-subrip"),
   }
-  resp = s.post(f"{host}/webservice/upload.php", data=data, files=files, proxies=proxies)
+  resp = s.post(f"{host}/webservice/upload.php", data=data, files=files)
   resp = json.loads(resp.text)[0]
   contextid, itemid, filename = resp["contextid"], resp["itemid"], resp["filename"]
   url = f"{host}/webservice/draftfile.php/{contextid}/user/draft/{itemid}/{urllib.parse.quote(filename)}?token={token}"
